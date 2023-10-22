1712a670479384c5f810b0eb1d21c0290869503f
Ryan Richard <tank1332@me.com>
2023-10-22 12:48:13 -0600

    Created another document for learnig in chapter 4

---
+++ Chapter4/dimensions.py
@@ -0,0 +1,14 @@
+dimensions = (200,50)
+print(dimensions)
+
+print("Original Dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,100)
+
+print("\nModified Dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+    
