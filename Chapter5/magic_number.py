3c8e110271b237fbeb355f3fc71e97eca92349e6
Ryan Richard <tank1332@me.com>
2023-10-22 22:34:23 -0600

    Added this here to test numerical and multiple comparisons and conditions.

---
+++ Chapter5/magic_number.py
@@ -0,0 +1,12 @@
+# Testing Numerical Comparisons
+magic_answer = 35
+if magic_answer != 42:
+    print("That is not the correct answer. Please try again!")
+
+# Testing Multiple Conditions
+age = 35
+print(age >= 21 and age<= 40)
+
+requested_topping = ['mushrooms','onions','pineapple']
+
+print('pepperoni' in requested_topping)
