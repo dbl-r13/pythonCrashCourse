e44623b5182764c6daebe9d2cfca6f70e8b75c3f
Ryan Richard <tank1332@me.com>
2023-10-22 22:35:03 -0600

    testing if else statements and how to use them together with different print statements based on conditions.

---
+++ Chapter5/voting.py
@@ -0,0 +1,12 @@
+age = 19
+def voting_age(age):
+    if age >=18:
+        print("You are old enough to vote!")
+        print("Have you registered to vote?")
+    else:
+        print("Sorry, you are too young to vote.")
+        print("Please register to vote as soon as you turn 18!")
+
+
+voting_age(13)
+voting_age(age)
